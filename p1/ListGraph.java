32e8b67848280afbe5e5c3b193424c0bcefa65f9
marvilion <marvilion@DESKTOP-2OJVI7A>
2024-09-20 19:24:30 -0400

    initial commit

---
+++ p1/ListGraph.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class ListGraph implements Graph {
+    private HashMap<String, LinkedList<String>> nodes = new HashMap<>();
+
+    public boolean addNode(String n) {
+	     throw new UnsupportedOperationException();
+    }
+
+    public boolean addEdge(String n1, String n2) {
+	     throw new UnsupportedOperationException();
+    }
+
+    public boolean hasNode(String n) {
+	     throw new UnsupportedOperationException();
+    }
+
+    public boolean hasEdge(String n1, String n2) {
+	     throw new UnsupportedOperationException();
+    }
+
+    public boolean removeNode(String n) {
+	     throw new UnsupportedOperationException();
+    }
+
+    public boolean removeEdge(String n1, String n2) {
+	     throw new UnsupportedOperationException();
+    }
+
+    public List<String> nodes() {
+	     throw new UnsupportedOperationException();
+    }
+
+    public List<String> succ(String n) {
+	     throw new UnsupportedOperationException();
+    }
+
+    public List<String> pred(String n) {
+	     throw new UnsupportedOperationException();
+    }
+
+    public Graph union(Graph g) {
+	     throw new UnsupportedOperationException();
+    }
+
+    public Graph subGraph(Set<String> nodes) {
+	     throw new UnsupportedOperationException();
+    }
+
+    public boolean connected(String n1, String n2) {
+	     throw new UnsupportedOperationException();
+    }
+}
